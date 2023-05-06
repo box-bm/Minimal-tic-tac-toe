0f55e59a208be865f3993e1a6ca1232526390bd7
Brandon Manzo <brandong.manzog@icloud.com>
2023-05-06 11:01:55 -0600

    created default players

---
+++ lib/repository/default_players.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:tik_tak_toe/models/player.dart';
+
+const Player defaultPlayer1 =
+    Player(color: Color.fromARGB(255, 238, 183, 18), iconData: Icons.close);
+
+const Player defaultPlayer2 = Player(
+    color: Color.fromARGB(255, 17, 161, 180), iconData: Icons.circle_outlined);
