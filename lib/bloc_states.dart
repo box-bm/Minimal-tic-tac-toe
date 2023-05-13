61c5c76fc8fd7958a9b0e480211ca92fab3d64de
Brandon Manzo <brandong.manzog@icloud.com>
2023-05-13 09:35:19 -0600

    change admob init location to main

--- lib/bloc_states.dart
+++ lib/bloc_states.dart
@@ -1,4 +1,3 @@
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:minimal_tic_tac_toe/common.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:minimal_tic_tac_toe/bloc/players/players_bloc.dart';
@@ -7,10 +6,6 @@ import 'package:minimal_tic_tac_toe/bloc/tic_tac_toe/tic_tac_toe_bloc.dart';
 class BlocAppStates extends StatelessWidget {
   final Widget child;
   const BlocAppStates({super.key, required this.child});
-
-  Future<InitializationStatus> _initGoogleMobileAds() {
-    return MobileAds.instance.initialize();
-  }
 
   @override
   Widget build(BuildContext context) {
