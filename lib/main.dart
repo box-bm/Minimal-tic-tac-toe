f6a6c0485292744b4e7e23e43da0e2ba85b5fede
Brandon Manzo <brandong.manzog@icloud.com>
2023-05-13 09:34:27 -0600

    change admob init location to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:minimal_tic_tac_toe/common.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
@@ -12,5 +13,6 @@ Future<void> main() async {
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   Directory dir = await getApplicationDocumentsDirectory();
   HydratedBloc.storage = await HydratedStorage.build(storageDirectory: dir);
+  MobileAds.instance.initialize();
   runApp(const TicTacToeApp());
 }
